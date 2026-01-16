5c56f3642ed317406ecc67e34f3a1ae04d61cf5b
Marco Edoardo Santimaria <marcoedoardo.santimaria@unito.it>
2026-01-16 14:49:01 +0000

    Initial code upload

---
+++ libcapio.hpp
@@ -0,0 +1,91 @@
+#ifndef PYCAPIO_PYCAPIO_HPP
+#define PYCAPIO_PYCAPIO_HPP
+
+/**
+ * Capio log level.
+ * if -1, and capio logging is enable everything is logged, otherwise, only
+ * logs up to CAPIO_MAX_LOG_LEVEL function calls
+ */
+inline bool syscall_no_intercept_flag;
+#define syscall_no_intercept syscall
+#define CAPIO_VERSION ""
+
+#include <array>
+#include <string>
+
+#include "common/syscall.hpp"
+
+#include "utils/clone.hpp"
+#include "utils/filesystem.hpp"
+#include "utils/snapshot.hpp"
+
+#include "handlers.hpp"
+
+inline void libcapio_init(const std::filesystem::path &CAPIO_DIR = ".",
+                          const std::string &CAPIO_APP_NAME      = CAPIO_DEFAULT_APP_NAME,
+                          const std::string &CAPIO_WORKFLOW_NAME = CAPIO_DEFAULT_WORKFLOW_NAME) {
+
+    if (getenv("CAPIO_APP_NAME") == nullptr) {
+        setenv("CAPIO_APP_NAME", CAPIO_APP_NAME.c_str(), 1);
+    }
+
+    if (getenv("CAPIO_DIR") == nullptr) {
+        setenv("CAPIO_DIR", CAPIO_DIR.string().c_str(), 1);
+    }
+
+    if (std::getenv("CAPIO_WORKFLOW_NAME") == nullptr) {
+        setenv("CAPIO_WORKFLOW_NAME", CAPIO_WORKFLOW_NAME.c_str(), 1);
+    }
+
+    std::cout << "LIBCAPIO | CAPIO_DIR: " << getenv("CAPIO_DIR") << std::endl;
+    std::cout << "LIBCAPIO | CAPIO_APP_NAME: " << getenv("CAPIO_APP_NAME") << std::endl;
+    std::cout << "LIBCAPIO | CAPIO_WORKFLOW_NAME: " << getenv("CAPIO_WORKFLOW_NAME") << std::endl;
+
+    init_filesystem();
+    initialize_new_thread();
+
+    START_SYSCALL_LOGGING();
+}
+
+inline void libcapio_teardown() { exit_handler(NULL, NULL, NULL, NULL, NULL, NULL, NULL); }
+
+inline auto libcapio_open(const char *path, int flags) {
+    START_LOG(gettid(), "call(path=%s)", path);
+    const long result = capio_openat(AT_FDCWD, path, flags, gettid());
+    LOG("open_handler result = %d", result);
+
+    if (result == CAPIO_POSIX_SYSCALL_SKIP) {
+        LOG("Delegating to glibc");
+        return open(path, flags);
+    }
+
+    if (result == CAPIO_POSIX_SYSCALL_ERRNO) {
+        LOG("Error has occurred while opening file from server side! errno=%s", strerror(errno));
+    }
+
+    return static_cast<int>(result);
+}
+
+inline auto libcapio_read(const int fd, char *buf, const size_t size) {
+    const long result = capio_read(fd, buf, size, gettid());
+    if (result == CAPIO_POSIX_SYSCALL_SKIP) {
+        return read(fd, buf, size);
+    }
+    return result;
+}
+
+inline auto libcapio_write(const int fd, const char *buf, const size_t size) {
+    long result = capio_write(fd, buf, size, gettid());
+    if (result == CAPIO_POSIX_SYSCALL_SKIP) {
+        return write(fd, buf, size);
+    }
+    return result;
+}
+
+inline auto libcapio_close(const int fd) {
+    long result;
+    close_handler(fd, NULL, NULL, NULL, NULL, NULL, &result);
+    return result;
+}
+
+#endif // PYCAPIO_PYCAPIO_HPP
---
+++ libcapio_example.cpp
@@ -0,0 +1,54 @@
+#include "libcapio.hpp"
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <vector>
+
+void print_usage(const char *prog_name) {
+    std::cout << "Usage:\n"
+              << "  " << prog_name << " --write <size_in_bytes>\n"
+              << "  " << prog_name << " --read <size_to_read>\n";
+}
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        print_usage(argv[0]);
+        return 1;
+    }
+
+    const std::string mode{argv[1]};
+    const size_t target_size{static_cast<size_t>(std::stoll(argv[2]))};
+    constexpr char file_path[]{"/tmp/test.dat"};
+
+    libcapio_init("/tmp");
+
+    if (mode == "--write") {
+        const std::vector<char> buffer(target_size, 'A');
+
+        const int f{libcapio_open(file_path, O_RDWR | O_CREAT)};
+        if (f >= 0) {
+            const ssize_t written = libcapio_write(f, buffer.data(), buffer.size());
+            std::cout << "Successfully wrote " << written << " bytes.\n";
+            libcapio_close(f);
+        } else {
+            std::cerr << "Error opening file for writing.\n";
+        }
+
+    } else if (mode == "--read") {
+        std::vector<char> recv_buffer(target_size);
+
+        const int f{libcapio_open(file_path, O_RDONLY)};
+        if (f >= 0) {
+            if (ssize_t bytes_read = libcapio_read(f, recv_buffer.data(), recv_buffer.size());
+                bytes_read > 0) {
+                std::cout << "Read " << bytes_read << " bytes from file.\n";
+            }
+            libcapio_close(f);
+        } else {
+            std::cerr << "Error opening file for reading: fd=" << f << "\n";
+        }
+    }
+
+    libcapio_teardown();
+    return 0;
+}
